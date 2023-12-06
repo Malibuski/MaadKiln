624d7d11e18fcb536908de57d503d5b2f3046c65
Scott Welch <cscottwelch2@gmail.com>
2023-12-06 15:26:51 -0500

    Initial Commit

---
+++ MaadKiln/MaadKilnApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaadKilnApp.swift
+//  MaadKiln
+//
+//  Created by Scott Welch on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MaadKilnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
