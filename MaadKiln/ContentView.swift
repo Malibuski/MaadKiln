caed12180b85fb5100bb66bd3d64c01cd29b594f
Scott Welch <cscottwelch2@gmail.com>
2023-12-07 09:35:45 -0500

    Testing changes to github

--- MaadKiln/ContentView.swift
+++ MaadKiln/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world! This is what I want to push to GitHub. Will it work?")
         }
         .padding()
     }
